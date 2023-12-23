701356b3f6166fb10c3c8daaed7966e67be6084f
momoessop <moessop@outlook.com>
2023-12-23 16:10:54 +0200

    intial commit

---
+++ Muhammed Saad CV/Pages/CV_PAGE.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Muhammed_Saad_CV.Pages
+{
+    public class CV_PAGEModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Muhammed Saad CV/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Muhammed_Saad_CV.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
